f88e508782fd9a8053c151ff7200cc98e0083495
Orășan Corina <62331686+corinaorasan@users.noreply.github.com>
2021-03-04 10:57:47 +0200

    Add timer (#3)
    * Uncomment the DessertTimer class. Reformat code
    
    * Initialize DessertTimer in onCreate
    
    * Start DessertTimer
    
    * Stop DessertTimer

--- app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     private var revenue = 0
     private var dessertsSold = 0
-
+    private lateinit var dessertTimer: DessertTimer
     // Contains all the views
     private lateinit var binding: ActivityMainBinding
 
@@ -73,7 +73,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         binding.dessertButton.setOnClickListener {
             onDessertClicked()
         }
-
+        dessertTimer = DessertTimer()
         // Set the TextViews to the right values
         binding.revenue = revenue
         binding.amountSold = dessertsSold
@@ -149,8 +149,9 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         return super.onOptionsItemSelected(item)
     }
 
-    override fun onStart(){
+    override fun onStart() {
         super.onStart()
+        dessertTimer.startTimer()
         Timber.i("onStart called")
     }
 
@@ -166,6 +167,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     override fun onStop() {
         super.onStop()
+        dessertTimer.stopTimer()
         Timber.i("onStop Called")
     }
 
